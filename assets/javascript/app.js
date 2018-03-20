7b51d1b01cb61e731ccccbd2c7fb027b1bbfd9f6
Zane P <ztpence@gmail.com>
2018-03-19 19:22:46 -0700

    psuedo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+// Get Firebase ready
+
+
+//Add click event for button to add train
+
+
+//Need to pull users input
+
+
+//create a local or temproary object to contain train user info
+
+
+
+// Need to create a way to plug user info to firebase
+
+
+//Need to create event to add new info to database insert train info to html maybe childsnapshot
+
+
+// Need to create logic for train next train arrival
+
